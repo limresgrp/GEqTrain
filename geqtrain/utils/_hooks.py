79a55300a28f9534a252d0e8db25485c17498888
marconobile <marconobile.007.mn@gmail.com>
2024-07-09 10:39:01 +0200

    wip on hooks

--- geqtrain/utils/_hooks.py
+++ geqtrain/utils/_hooks.py
@@ -2,16 +2,12 @@ import torch
 import torch.nn as nn
 from functools import partial
 import warnings
-from typing import Callable, Optional, Union, Tuple, List
+from typing import Callable, Optional, Union, Tuple, List, Dict
 import numpy as np
 import wandb
 
 
-'''
-To create a new hook function you must register it into function_mappings dict as:
-- k: name of the method, this key is parsed as string from the yaml
-- value, the actual python method
-'''
+
 
 
 ##################
@@ -41,39 +37,36 @@ To create a new hook function you must register it into function_mappings dict a
 #             print(f"Cant hook on {name}")
 #             pass
 
-#     self.hooks = hooks
+#     self.hookss = hooks
 
 # def deregister_hooks(self):
-#     for hook in self.hooks:
+#     for hook in self.hookss:
 #         hook.remove()
 
-################################################
 
+# def gather_stats(i, filter_in, filter_out, mod, inp, out):
+#     '''
+#     filter: type of layer to skip
 
-def gather_stats(i, filter_in, filter_out, mod, inp, out):
-    '''
-    filter: type of layer to skip
+#     '''
+#     if mod.__class__.__name__ in filter_out:
+#         return
 
-    '''
-    if mod.__class__.__name__ in filter_out:
-        return
+#     # try:
+#     name = f"{i}_{mod.__class__.__name__}"
+#     if str(mod.__class__.__name__) in filter_in:
+#         acts = out.detach()
+#         # wandb.log({f"{name}_mean": acts.mean()})
+#         # wandb.log({f"{name}_std": acts.std()})
+#         # wandb.log({f"{name}_%_dead": acts.abs().histc(40,0,10)})   # adds hist of abs vals of activations, 50 bins
+#         hist = np.histogram(acts.to('cpu'))
+#         wandb.log({f"{name}_dist": wandb.Histogram(np_histogram=hist)})
 
-    # try:
-    name = f"{i}_{mod.__class__.__name__}"
-    if str(mod.__class__.__name__) in filter_in:
-        acts = out.detach()
-        # wandb.log({f"{name}_mean": acts.mean()})
-        # wandb.log({f"{name}_std": acts.std()})
-        # wandb.log({f"{name}_%_dead": acts.abs().histc(40,0,10)})   # adds hist of abs vals of activations, 50 bins
-        hist = np.histogram(acts.to('cpu'))
-        wandb.log({f"{name}_dist": wandb.Histogram(np_histogram=hist)})
-
-    # except:
-    #     pass
+#     # except:
+#     #     pass
 
 
 #####################################################
-
 
 
 def get_all_modules(model):
@@ -117,8 +110,6 @@ def get_all_hookable_modules(model):
 
 def print_stats(hook_handler, module_id, module_name, module, input, output):
 
-    # TODO HISTOGRAMS and wandb logging
-
     def print_tensor(t):
         return f"shape: {t.shape}, mean: {t.mean().item()}, std: {t.std().item()}, min: {t.min().item()}, max {t.max().item()}"
 
@@ -135,6 +126,44 @@ def print_stats(hook_handler, module_id, module_name, module, input, output):
     print(f"ModuleID: {module_id} of type {module} in {module_name}")
     print("Input: ", print_tensor(input))
     print("Output ", print_tensor(output), "\n")
+
+    if not hasattr(hook_handler, "stats"):
+        hook_handler.stats = {} # stores means and stds of hookable layers
+
+    key = f"{module_id}_{module_name}"
+
+    # wandb.log({f"{module_id}_{module_name}":
+    #          wandb.Histogram(output.tolist())})
+
+    if key not in hook_handler.stats:
+        hook_handler.stats[key] = {'output_mean': [], 'output_std': []}
+
+    hook_handler.stats[key]['output_mean'].append(output.mean().item())
+    hook_handler.stats[key]['output_std'].append(output.std().item())
+
+    columns = ['output_mean', 'output_std']
+    n = len(hook_handler.stats[key]['output_mean'])
+    xs = list(range(n))
+    ys = [hook_handler.stats[key]['output_mean'], hook_handler.stats[key]['output_std']]
+
+    wandb.log(
+        {f"{key}" :wandb.plot.line_series(
+            xs=xs,
+            ys=ys,
+            keys=columns,
+            title=f"Activations mean/std {key}")
+        }
+    )
+
+
+'''
+To create a new hook function you must register it into function_mappings dict as:
+- k: name of the method, this key is parsed as string from the yaml
+- value, the actual python method
+'''
+available_hooks = {
+    "print_stats" : print_stats,
+}
 
 
 class ForwardHookHandler:
@@ -155,33 +184,43 @@ class ForwardHookHandler:
         - input: the input of the hooked module; input is always a tuple with: (input,)
         - output: the output of the hooked module
     '''
-    def __init__(self, trainer, hook_func: callable, filter_in: List = [], filter_out: List = []):
-        # self.model = model
-        # self.hook_func: Callable = hook_func
+    def __init__(self, trainer, callables: List[Dict] = []):
         model = trainer.model
-        self.hook = []
-        hookable_layers: List = get_all_hookable_modules(model)
-        clean_module_name = lambda s : s.split('(')[0].lower()
-        filter_in  = [clean_module_name(s) for s in filter_in]
-        filter_out = [clean_module_name(s) for s in filter_out]
+        self.hooks = []
+        self.hookable_layers: List = get_all_hookable_modules(model)
+        self.clean_module_name = lambda s : s.split('(')[0].lower()
 
-        for (id, name, layer) in hookable_layers:
-            is_module_in_filter_in = clean_module_name(str(layer)) in filter_in
-            is_module_in_filter_out = clean_module_name(str(layer)) in filter_out
+        for hook_data in callables:
+            callable_name, filter_in, filter_out = hook_data["callable_name"], hook_data["filter_in"], hook_data["filter_out"]
+            filter_in = filter_in or []
+            filter_out = filter_out or []
+            callable_ = available_hooks.get(str(callable_name), None)
+            if callable_:
+                self._register_hook(
+                    hook_func  =  callable_,
+                    filter_in  = filter_in,
+                    filter_out = filter_out,
+                )
+
+    def _register_hook(self, hook_func: callable, filter_in: List = [], filter_out: List = []):
+        filter_in  = [self.clean_module_name(s) for s in filter_in]
+        filter_out = [self.clean_module_name(s) for s in filter_out]
+
+        for (id, name, layer) in self.hookable_layers:
+            is_module_in_filter_in = self.clean_module_name(str(layer)) in filter_in
+            is_module_in_filter_out = self.clean_module_name(str(layer)) in filter_out
             if is_module_in_filter_out:
                 continue
             elif (not filter_in) or is_module_in_filter_in:
                 callable = partial(hook_func, self, id, name)
-                self.hook.append(layer.register_forward_hook(callable))
+                self.hooks.append(layer.register_forward_hook(callable))
             else:
-                raise ValueError(f'IDK what to do with {clean_module_name(str(layer))} (aka {str(layer)})')
+                raise ValueError(f'IDK what to do with {self.clean_module_name(str(layer))} (aka {str(layer)})')
 
     def deregister_hooks(self):
-        for hook in self.hook:
+        for hook in self.hooks:
             hook.remove()
 
     def __del__(self):
-        for hook in self.hook:
-            hook.remove()
-
+        self.deregister_hooks()
 
