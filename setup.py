a0b8481133b65102b47a5cf109bd0d1fba251211
Daniangio <daniele.angioletti0@gmail.com>
2024-08-06 10:47:45 +0200

    Included plotly package in setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "wandb>=0.13",
         "e3nn>=0.4.4,<0.6.0",
         "pyyaml",
+        "plotly",
         "contextlib2;python_version<'3.7'",  # backport of nullcontext
         'contextvars;python_version<"3.7"',  # backport of contextvars for savenload
         "typing_extensions;python_version<'3.8'",  # backport of Final
