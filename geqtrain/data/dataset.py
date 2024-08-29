Corrected normalization in interaction layer + minor fixes

geqtrain/data/dataset.py
@@ -27,6 +27,8 @@ from .AtomicData import _process_dict
 
 
 def fix_batch_dim(arr):
+    if arr is None:
+        return None
     if len(arr.shape) == 0:
         return arr.reshape(1)
     return arr
@@ -475,7 +477,7 @@ def parse_attrs(
                     _input_type[mask] = 0
             else:
                 if val is None:
-                    _input_type = np.array([-1])
+                    val = np.array([-1])
                 _input_type = val + 1
                 mask = np.isnan(val)
                 _input_type[mask] = 0

geqtrain/nn/_fc.py
@@ -2,8 +2,7 @@
 """
 
 import torch
-from torch import _weight_norm, norm_except_dim
-from torch import fx
+from torch.nn.utils import weight_norm
 from typing import List, Optional
 from math import sqrt
 from collections import OrderedDict
@@ -22,7 +21,7 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
             out_features (int): The number of output features from the MLP.
             use_weight_norm (bool): Flag indicating whether weight normalization is used.
             dim_weight_norm (int): Dimension along which to apply weight normalization.
-            use_norm_layer (bool): Flag indicating whether normalization layers are used.
+            use_layer_norm (bool): Flag indicating whether normalization layers are used.
 
         Methods:
             __init__: Initializes the MLP with the specified parameters.
@@ -33,7 +32,7 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
             mlp_latent_dimensions (List[int]): List of dimensions for the hidden layers.
             mlp_output_dimension (Optional[int]): Dimension of the output of the MLP.
             mlp_nonlinearity (Optional[str]): Type of non-linearity to use ('silu', 'ssp', 'selu', or None).
-            use_norm_layer (bool): Whether to use layer normalization.
+            use_layer_norm (bool): Whether to use layer normalization.
             use_weight_norm (bool): Whether to use weight normalization.
             dim_weight_norm (int): Dimension along which to apply weight normalization.
             has_bias (bool): Whether the linear layers have bias.
@@ -51,7 +50,7 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
                 mlp_latent_dimensions=[256, 256],
                 mlp_output_dimension=10,
                 mlp_nonlinearity='silu',
-                use_norm_layer=True,
+                use_layer_norm=True,
                 use_weight_norm=True,
                 dim_weight_norm=1,
                 has_bias=True,
@@ -63,7 +62,7 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
 
     in_features: int
     out_features: int
-    use_norm_layer: bool
+    use_layer_norm: bool
     use_weight_norm: bool
     dim_weight_norm: int
 
@@ -73,8 +72,8 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
         mlp_latent_dimensions: List[int],
         mlp_output_dimension: Optional[int],
         mlp_nonlinearity: Optional[str] = "silu",
-        use_norm_layer: bool = False,
-        use_weight_norm: bool = False,
+        use_layer_norm: bool = False,
+        use_weight_norm: bool = True,
         dim_weight_norm: int = 0,
         has_bias: bool = False,
         bias: Optional[List] = None,
@@ -110,15 +109,22 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
         self.out_features = dimensions[-1]
         self.use_weight_norm = use_weight_norm
         self.dim_weight_norm = dim_weight_norm
-        self.use_norm_layer = use_norm_layer
+        self.use_layer_norm = use_layer_norm
 
-        sequential_dict = OrderedDict()
-        
         if bias is not None:
             has_bias = True
+        
+        sequential_dict = OrderedDict()
+
+        if self.use_layer_norm:
+            sequential_dict['norm'] = torch.nn.LayerNorm(dimensions[0])
 
         for layer, (h_in, h_out) in enumerate(zip(dimensions, dimensions[1:])):
             lin_layer = torch.nn.Linear(h_in, h_out, bias=has_bias)
+
+            # Apply weight normalization if specified
+            if self.use_weight_norm:
+                lin_layer = weight_norm(lin_layer, name='weight', dim=self.dim_weight_norm)
 
             is_last_layer = num_layers - 1 == layer
             if (nonlinearity is not None) and (not is_last_layer):
@@ -135,13 +141,10 @@ class ScalarMLPFunction(CodeGenMixin, torch.nn.Module):
                     # as in: https://pytorch.org/docs/stable/nn.init.html#torch.nn.init.kaiming_normal_
                     lin_layer.weight = lin_layer.weight.normal_(0, nonlin_const / sqrt(float(h_in)))
                 
-                layers = []
-                if self.use_norm_layer:
-                    layers.append('norm', torch.nn.LayerNorm(h_in))
-                layers.extend([
+                layers = [
                         ("linear", lin_layer),
                         ("activation", non_lin_instance),
-                ])
+                ]
                 sequential_dict[f"{layer}_activated"] = torch.nn.Sequential(OrderedDict(layers))
 
             else:

geqtrain/nn/interaction.py
@@ -3,7 +3,7 @@ import functools
 import torch
 import torch.nn.functional as F
 
-from typing import Callable, Optional, List, Tuple, Union
+from typing import Optional, List, Tuple, Union
 from torch_scatter import scatter
 from torch_scatter.composite import scatter_softmax
 
@@ -456,19 +456,19 @@ class InteractionLayer(torch.nn.Module):
         self.isqrtd = math.isqrt(self.head_dim)
 
         # Make the env embed linear, which mixes eq. feats after edges scatter over nodes
-        env_norm = SO3_LayerNorm(
+        self.env_norm = SO3_LayerNorm(
             env_embed_irreps,
         )
-        env_linear = SO3_Linear(
+        self.env_linear = SO3_Linear(
             env_embed_irreps,
             env_embed_irreps,
             bias=True,
         )
 
-        product, reshape_in_module = None, None
+        self.product, self.reshape_in_module = None, None
         if parent.use_mace_product:
             # Perform eq. Atomic Cluster Expansion
-            product = EquivariantProductBasisBlock(
+            self.product = EquivariantProductBasisBlock(
                 node_feats_irreps=env_embed_irreps,
                 target_irreps=env_embed_irreps,
                 correlation=product_correlation,
@@ -476,7 +476,7 @@ class InteractionLayer(torch.nn.Module):
             )
 
             # Reshape back product so that you can perform tp: n m d -> n (m d)
-            reshape_in_module = reshape_irreps(env_embed_irreps)
+            self.reshape_in_module = reshape_irreps(env_embed_irreps)
 
         # Make TP
         l_arg_irreps, l_out_irreps = tps_irreps
@@ -497,12 +497,8 @@ class InteractionLayer(torch.nn.Module):
         full_out_irreps = o3.Irreps(full_out_irreps)
         assert all(ir == SCALAR for _, ir in full_out_irreps[:tp_n_scalar_outs])
 
-        self.tp_input_1_norm = SO3_LayerNorm(l_arg_irreps)
-        # self.tp_input_1_norm = EquivariantNormLayer("layer_norm_sh", max(l_arg_irreps.ls), self.env_embed_multiplicity)
-        self.tp_input_2_norm = SO3_LayerNorm(l_arg_irreps)
-        # self.tp_input_2_norm = EquivariantNormLayer("layer_norm_sh", max(l_arg_irreps.ls), self.env_embed_multiplicity)
         # Build tensor product between env-aware node feats and edge attrs
-        tp = Contracter(
+        self.tp = Contracter(
             irreps_in1=o3.Irreps(
                 [(self.env_embed_multiplicity, ir) for _, ir in l_arg_irreps]
             ),
@@ -552,8 +548,7 @@ class InteractionLayer(torch.nn.Module):
         _features_n_scalar_outs = linear_out_irreps.count(SCALAR) // linear_out_irreps[0].mul
         parent._features_n_scalar_outs.append(_features_n_scalar_outs)
 
-        is_last_layer = True if self.layer_index+1 == parent.num_layers else False
-        linear = SO3_Linear(
+        self.linear = SO3_Linear(
             full_out_irreps,
             linear_out_irreps,
             bias=True,
@@ -571,6 +566,7 @@ class InteractionLayer(torch.nn.Module):
                     )
                 ),
                 mlp_output_dimension=None,
+                use_layer_norm=True,
             )
             self._latent_dim = latent_mlp.out_features
         else:
@@ -584,21 +580,19 @@ class InteractionLayer(torch.nn.Module):
                     + self.env_embed_multiplicity * parent._tp_n_scalar_outs[self.layer_index - 1]
                 ),
                 mlp_output_dimension=self._latent_dim,
+                use_layer_norm=False,
             )
-        
-        latents_norm = SO3_LayerNorm(
-            o3.Irreps(f'{self._latent_dim}x0e'),
-        )
 
         # the env embed MLP takes the last latent's output as input
         # and outputs enough weights for the env embedder
-        env_embed_mlp = env_embed(
+        self.env_embed_mlp = env_embed(
             mlp_input_dimension=self._latent_dim,
             mlp_output_dimension=generate_n_weights,
+            has_bias=False,
         )
 
         # Take the node attrs and obtain a query matrix
-        edge_attr_to_query = ScalarMLPFunction(
+        self.edge_attr_to_query = ScalarMLPFunction(
             mlp_input_dimension=(
                  # Node invariants for center and neighbor (chemistry)
                 2 * parent.irreps_in[parent.node_invariant_field].num_irreps
@@ -608,36 +602,23 @@ class InteractionLayer(torch.nn.Module):
             mlp_latent_dimensions = [],
             mlp_output_dimension=self.env_embed_multiplicity * self.head_dim,
             mlp_nonlinearity = None,
-            use_norm_layer = True,
             zero_init_last_layer_weights= True,
         ) if parent.use_attention else None
 
         # Take the node attrs and obtain a query matrix
-        latent_to_key = ScalarMLPFunction(
+        self.latent_to_key = ScalarMLPFunction(
             mlp_input_dimension = self._latent_dim,
             mlp_latent_dimensions = [],
             mlp_output_dimension = self.env_embed_multiplicity * self.head_dim,
             mlp_nonlinearity = None,
-            use_norm_layer = False,
             zero_init_last_layer_weights = True,
         ) if parent.use_attention else None
 
         self.latent_mlp = latent_mlp
-        self.latents_norm = latents_norm
-        self.env_embed_mlp = env_embed_mlp
         self._env_weighter = env_weighter
-        self.edge_attr_to_query = edge_attr_to_query
-        self.latent_to_key = latent_to_key
-        self.env_norm = env_norm
-        self.env_linear = env_linear
-        self.product = product
-        self.reshape_in_module = reshape_in_module
-        self.tp = tp
         self.tp_n_scalar_out = parent._tp_n_scalar_outs[self.layer_index]
-        self.linear = linear
         self.use_attention = parent.use_attention
         self.use_mace_product = parent.use_mace_product
-        self.is_last_layer = is_last_layer
 
         if not parent.use_attention:
             self.register_buffer(
@@ -664,8 +645,6 @@ class InteractionLayer(torch.nn.Module):
 
         # Compute latents
         new_latents = self.latent_mlp(inv_latent_cat)
-        new_latents = self.latents_norm(new_latents.unsqueeze(-1)).squeeze(-1)
-
         # Apply cutoff, which propagates through to everything else
         new_latents = cutoff_coeffs.unsqueeze(-1) * new_latents
 
@@ -766,10 +745,6 @@ class InteractionLayer(torch.nn.Module):
         # Copy to get per-edge
         # Large allocation, but no better way to do this:
         local_env_per_active_edge = expanded_features_per_node[edge_center]
-
-        # Normalize equivariant features before tp
-        eq_features = self.tp_input_1_norm(eq_features)
-        local_env_per_active_edge = self.tp_input_2_norm(local_env_per_active_edge)
 
         # Now do the TP
         # recursively tp current features with the environment embeddings

geqtrain/nn/so3.py
@@ -205,6 +205,10 @@ class SO3_LayerNorm(torch.nn.Module):
             torch.Tensor: Output tensor after linear transformation and optional bias addition.
         '''
 
+        orig_shape = x.shape
+        if len(orig_shape) == 2:
+            x = x.unsqueeze(-1)
+
         out = []
         start = 0
         l_start = 0
@@ -224,7 +228,7 @@ class SO3_LayerNorm(torch.nn.Module):
 
             feature_norm = torch.mean(feature_norm, dim=2, keepdim=True)    # [N, 1, 1]
             # Feattures whose feature_norm is < 1.e-3 are not normalized (thus they are let die)
-            feature_norm = torch.clamp_max_((feature_norm + self.eps).pow(-0.5) * self.l_dim_norm, 1.e-3)
+            feature_norm = (feature_norm + self.eps).pow(-0.5) * self.l_dim_norm
             feature = feature * feature_norm
 
             feature = rearrange(feature, 'b l (m i) -> b m (i l)', m=self.mul, l=_l_dims[0], i=len(_l_dims))
@@ -237,6 +241,8 @@ class SO3_LayerNorm(torch.nn.Module):
         if self.bias is not None:
             out[..., :sum(self.l_dims[0])] += self.bias.unsqueeze(0)
 
+        if out.shape != orig_shape:
+            out = out.reshape(*orig_shape)
         return out
 
     def __repr__(self):