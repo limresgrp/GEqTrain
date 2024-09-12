95d4f2d951ba9f62f4522bb333ab313bb74cd603
Daniangio <daniele.angioletti0@gmail.com>
2024-09-12 10:00:36 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-# conda create --name geqtrain python=3.10
-conda install pytorch==1.13 pytorch-cuda=11.7 -c pytorch -c nvidia
-pip install torch-scatter
-pip install -e .
+pip3 install torch==2.4.0 --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch-scatter -f https://data.pyg.org/whl/torch-2.4.0+cu118.html
+pip3 install -e .
