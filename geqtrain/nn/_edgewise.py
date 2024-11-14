a8225ba5b015372222575b9a13b13b0b29ccbe51
marconobile <marconobile.007.mn@gmail.com>
2024-11-14 11:15:46 +0100

    add: assert in edgewise over num_nodes

--- geqtrain/nn/_edgewise.py
+++ geqtrain/nn/_edgewise.py
@@ -101,6 +101,7 @@ class EdgewiseReduce(GraphModuleMixin, torch.nn.Module):
 
         species = data[AtomicDataDict.NODE_TYPE_KEY].squeeze(-1)
         num_nodes = len(species)
+        assert num_nodes == data['pos'].shape[0]
 
         if self.use_attention and self.node_attr_to_query is not None:
             Q = self.node_attr_to_query(data[AtomicDataDict.NODE_ATTRS_KEY][edge_center])
@@ -118,7 +119,5 @@ class EdgewiseReduce(GraphModuleMixin, torch.nn.Module):
         # aggregation step
         data[self.out_field] = scatter(edge_feat, edge_center, dim=0, dim_size=num_nodes)
 
-        if not self.use_attention:
-            data[self.out_field] *= self.env_sum_normalization
-
+        if not self.use_attention: data[self.out_field] *= self.env_sum_normalization
         return data
