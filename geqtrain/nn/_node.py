acae2c89a595474ceb1e30752d8bf2098c5e1342
Daniangio <daniele.angioletti0@gmail.com>
2025-03-26 17:39:56 +0100

    _node is not jit compilable

--- geqtrain/nn/_node.py
+++ geqtrain/nn/_node.py
@@ -6,7 +6,7 @@ from e3nn.util.jit import compile_mode
 from geqtrain.data import AtomicDataDict
 from ._graph_mixin import GraphModuleMixin
 
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 @compile_mode("script")
 class EmbeddingNodeAttrs(GraphModuleMixin, torch.nn.Module):
@@ -25,50 +25,60 @@ class EmbeddingNodeAttrs(GraphModuleMixin, torch.nn.Module):
         super().__init__()
 
         irreps_in = {} if irreps_in is None else irreps_in
-        numerical_attrs = []
-        categorical_attr_modules = torch.nn.ModuleDict() # k: str field name, v: nn.Embedding layer
+        self._numerical_attrs: List[str] = []
+        self._categorical_attr_modules = torch.nn.ModuleDict() # k: str field name, v: nn.Embedding layer
         output_embedding_dim = 0
-        for field, values in node_attributes.items():
 
+        for field, values in node_attributes.items():
             if 'embedding_dimensionality' not in values: # this means the attr is not used as embedding
                 continue
 
             irreps_in[field] = None
             embedding_dim = values['embedding_dimensionality']
+            
             if values.get('attribute_type', 'categorical') == 'numerical':
-                numerical_attrs.append(field)
+                self._numerical_attrs.append(field)
             else:
                 n_types = values.get('actual_num_types', num_types)
-                embedding_dim = values['embedding_dimensionality']
                 emb_module = torch.nn.Embedding(n_types, embedding_dim)
-                # torch.nn.init.xavier_uniform_(emb_module.weight) # with option 3 below?
                 torch.nn.init.normal_(emb_module.weight, mean=0, std=1.0) # options: 1) std=1 2) math.isqrt(embedding_dim) 3) 0.3333*math.isqrt(embedding_dim) as in https://github.com/bigscience-workshop/bigscience/blob/master/train/tr11-176B-ml/chronicles.md
-                categorical_attr_modules[field] = emb_module
 
-            if stable_embedding:
-                categorical_attr_modules[field] = torch.nn.Sequential(emb_module, torch.nn.LayerNorm(embedding_dim)) # as in: https://huggingface.co/docs/bitsandbytes/main/en/reference/nn/embeddings#bitsandbytes.nn.StableEmbedding
+                if stable_embedding:
+                    emb_module = torch.nn.Sequential(
+                        emb_module,
+                        torch.nn.LayerNorm(embedding_dim)
+                    ) # as in: https://huggingface.co/docs/bitsandbytes/main/en/reference/nn/embeddings#bitsandbytes.nn.StableEmbedding
+
+                self._categorical_attr_modules[field] = emb_module
 
             output_embedding_dim += embedding_dim
 
-        self.numerical_attrs = numerical_attrs
-        self.categorical_attr_modules = categorical_attr_modules
-        irreps_out = {AtomicDataDict.NODE_ATTRS_KEY: Irreps([(output_embedding_dim, (0, 1))])} # output_embedding_dim scalars (l=0) with even parity
+        self._has_numericals = len(self._numerical_attrs) > 0
+
+        # Define Irreps output
+        irreps_out = {
+            AtomicDataDict.NODE_ATTRS_KEY: Irreps([(output_embedding_dim, (0, 1))]) # output_embedding_dim scalars (l=0) with even parity
+        }
+
+        # Initialize irreps
         self._init_irreps(irreps_in=irreps_in, irreps_out=irreps_out)
 
 
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
-        with torch.cuda.amp.autocast(enabled=False): # choice: embeddings are always kept to high precision, regardless of amp
+        with torch.amp.autocast('cuda', enabled=False): # choice: embeddings are always kept to high precision, regardless of amp
             out = []
-            for attribute_name, emb_layer in self.categorical_attr_modules.items():
+            for attribute_name, emb_layer in self._categorical_attr_modules.items():
                 x = data[attribute_name].squeeze()
                 x = emb_layer(x)
                 out.append(x)
 
-        for attribute_name in self.numerical_attrs:
-            x = data[attribute_name]
-            out.append(x)
+        if self._has_numericals:
+            assert hasattr(self, '_numerical_attrs') # Needed to jit compile
+            for attribute_name in self._numerical_attrs:
+                x = data[attribute_name]
+                out.append(x)
 
-        data[AtomicDataDict.NODE_ATTRS_KEY] = torch.cat(out, dim=-1).float()
+            data[AtomicDataDict.NODE_ATTRS_KEY] = torch.cat(out, dim=-1).float()
         return data
 
 
