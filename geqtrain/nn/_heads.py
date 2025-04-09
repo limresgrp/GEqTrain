fix: update autocast decorator to use torch.amp for high precision in attention and embedding layers

geqtrain/nn/_heads.py
@@ -312,7 +312,7 @@ class L0IndexedAttention(GraphModuleMixin, nn.Module):
         padded_edge_attrs = rearrange(padded_edge_attrs, 'batch target source heads -> batch heads target source')
         return padded_edge_attrs
 
-    @torch.cuda.amp.autocast(enabled=False) # attention always kept to high precision, regardless of AMP
+    @torch.amp.autocast('cuda', enabled=False) # attention always kept to high precision, regardless of AMP
     def forward(self, features, data):
         # forward logic: https://rbcborealis.com/wp-content/uploads/2021/08/T17_7.png
         N, emb_dim = features.shape # N = num nodes or num ensemble confs

geqtrain/nn/_node.py
@@ -97,7 +97,7 @@ class EmbeddingAttrs(GraphModuleMixin, torch.nn.Module):
         irreps_out = {self.out_field: Irreps([(output_embedding_dim, (0, 1))])} # output_embedding_dim scalars (l=0) with even parity
         self._init_irreps(irreps_in=irreps_in, irreps_out=irreps_out)
 
-    @torch.cuda.amp.autocast(enabled=False) # embeddings always kept to high precision, regardless of AMP
+    @torch.amp.autocast('cuda', enabled=False) # embeddings always kept to high precision, regardless of AMP
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         out = []
         for attribute_name, emb_layer in self.attr_modules.items():