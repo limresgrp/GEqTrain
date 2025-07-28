84d3789c2d31fc8482c51418ff857381c417c86b
Daniangio <daniele.angioletti0@gmail.com>
2025-07-28 12:42:56 +0200

    Common method to process out_irreps for interaction and MACE + Update MACE model

--- geqtrain/model/_mace_model.py
+++ geqtrain/model/_mace_model.py
@@ -1,51 +1,29 @@
 import logging
-from geqtrain.data import AtomicDataDict
+from typing import Optional
+from geqtrain.model._embedding import buildEmbeddingLayers
 from geqtrain.utils import Config
-from geqtrain.nn import (
-    SequentialGraphNetwork,
-    EmbeddingInputAttrs,
-    SphericalHarmonicEdgeAngularAttrs,
-    BasisEdgeRadialAttrs,
-    MACEModule,
-)
+from geqtrain.nn import SequentialGraphNetwork, MACEModule
 
 
 
-def MACEModel(config:Config) -> SequentialGraphNetwork:
+def MACEModel(config:Config, model: Optional[SequentialGraphNetwork]) -> SequentialGraphNetwork:
     """Base model architecture.
 
     """
-    layers = buildMACEModelLayers(config)
+    layers = buildEmbeddingLayers(config, model)
+    layers.update(buildMACEModelLayers())
 
     return SequentialGraphNetwork.from_parameters(
         shared_params=config,
         layers=layers,
     )
 
-def buildMACEModelLayers(config:Config):
-    logging.info("--- Building Node Model ---")
+def buildMACEModelLayers():
+    logging.info("--- Building MACE Model ---")
 
-    from geqtrain.data._build import update_config
-    update_config(config)
-
+    
     layers = {
-        "node_attrs": (EmbeddingInputAttrs, dict(
-            out_field=AtomicDataDict.NODE_ATTRS_KEY,
-            attributes=config.get('node_attributes'),
-        )),
-    }
-
-    if 'edge_attributes' in config:
-        edge_embedder = (EmbeddingInputAttrs, dict(
-            out_field=AtomicDataDict.EDGE_FEATURES_KEY,
-            attributes=config.get('edge_attributes'),
-        ))
-        layers["edge_attrs"] = edge_embedder
-
-    layers.update({
-        "edge_radial_attrs":  BasisEdgeRadialAttrs,
-        "edge_angular_attrs": SphericalHarmonicEdgeAngularAttrs,
         "interaction": MACEModule,
-    })
+    }
 
     return layers
