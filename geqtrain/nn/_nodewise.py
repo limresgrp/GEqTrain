a7926dc444efe08d913ff2f9160cef835611f73c
Daniangio <daniele.angioletti0@gmail.com>
2025-07-30 16:52:22 +0200

    Allow to specify a bias to apply to scalar output after node pooling

--- geqtrain/nn/_nodewise.py
+++ geqtrain/nn/_nodewise.py
@@ -1,6 +1,5 @@
 import torch
-from typing import Optional
-# from torch_scatter import scatter
+from typing import List, Optional, Union
 from geqtrain.utils.pytorch_scatter import scatter_sum
 
 from geqtrain.data import AtomicDataDict
@@ -8,9 +7,7 @@ from geqtrain.nn import GraphModuleMixin
 
 
 class NodewiseReduce(GraphModuleMixin, torch.nn.Module):
-    """Sum nodewise features.
-
-    """
+    """Sum nodewise features."""
 
     out_field: str
 
@@ -20,8 +17,9 @@ class NodewiseReduce(GraphModuleMixin, torch.nn.Module):
         out_field: Optional[str] = None,
         irreps_in={},
         residual_field: Optional[str] = None,
+        bias: Optional[Union[float, List, torch.Tensor]] = None,
     ):
-        """Sum edges into nodes."""
+        """Sum edges into nodes, with optional bias."""
         super().__init__()
         self.field = field
         self.out_field = f"sum_{field}" if out_field is None else out_field
@@ -38,6 +36,20 @@ class NodewiseReduce(GraphModuleMixin, torch.nn.Module):
             else {},
         )
 
+        # Handle bias: can be float, list of floats, or torch.Tensor
+        if bias is not None:
+            if isinstance(bias, (float, int)):
+                bias_tensor = torch.tensor([bias], dtype=torch.get_default_dtype())
+            elif isinstance(bias, list):
+                bias_tensor = torch.tensor(bias, dtype=torch.get_default_dtype())
+            elif isinstance(bias, torch.Tensor):
+                bias_tensor = bias
+            else:
+                raise TypeError("Bias must be float, list of floats, or torch.Tensor")
+            self.bias = torch.nn.Parameter(bias_tensor)
+        else:
+            self.bias = None
+
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         data = AtomicDataDict.with_batch(data)
         node_feat: torch.Tensor = data[self.field]
@@ -47,5 +59,8 @@ class NodewiseReduce(GraphModuleMixin, torch.nn.Module):
             node_feat = torch.cat((node_feat, data[self.residual_field]), dim=-1)
 
         if batch is not None:
-            data[self.out_field] = scatter_sum(node_feat, batch, dim=0)
+            pooled = scatter_sum(node_feat, batch, dim=0)
+            if self.bias is not None:
+                pooled = pooled + self.bias
+            data[self.out_field] = pooled
         return data
